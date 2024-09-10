73c2e036c30931fa29e99038401293b6fe2efba0
Sergey <lipatovsergey01@gmail.com>
2024-09-11 00:21:43 +0300

    1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,7 +7,6 @@ const Statistics = (props) => {
 
   return (
     <>
-      <h2>statics</h2>
       <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
@@ -23,7 +22,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const allFeedBack = good + neutral + bad
 
   return (
     <>
@@ -32,7 +31,12 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <h2>statics</h2>
+      {allFeedBack > 0 ? 
+        (<Statistics good={good} neutral={neutral} bad={bad}/>)
+        :
+        (<p>No feedback given</p>)
+      }
     </>
   )
 }
